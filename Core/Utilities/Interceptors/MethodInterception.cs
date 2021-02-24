f60766f2992e1036fa52163ebeb432edb743dc09
Enes ÜLKÜ <enesulku4834@gmail.com>
2021-02-24 13:15:05 +0300

    ValidationAspect added

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,41 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation, System.Exception e) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+        public override void Intercept(IInvocation invocation)
+        {
+            //hata ayıklama  invocation gelen metot
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+
+}
